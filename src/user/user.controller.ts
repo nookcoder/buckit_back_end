0d556c4fcf65e52588afa8ef4489c6919aee8b36
wook <gus5427@naver.com>
2022-06-18 02:25:30 +0900

    Feat : add feature thar get user by jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CoreOutput } from '../common/dto/core-output.dto';
 import { CreateUserInput } from './dto/create-user.dto';
 import { CheckEmail, CheckPhone } from './dto/check-exist-user.dto';
+import { JwtAuthGuard } from '../auth/passport/jwt/jwt-auth.guard';
 
-@Controller('user')
+@Controller('api/v1/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/profile')
+  async getProfile(@Request() req) {
+    return await this.userService.getProfileById(req.user.userId);
+  }
+
   // 존재하는 유저인지 확인
   @Post('/check')
   async checkUserPhoneNumber(
