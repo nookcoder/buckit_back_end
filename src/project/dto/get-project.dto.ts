4d54264c5facc48809dfb5b992c2af4540f2f306
wook <gus5427@naver.com>
2022-06-24 23:44:53 +0900

    Feat : create get-project.dto.ts

---
+++ src/project/dto/get-project.dto.ts
@@ -0,0 +1,10 @@
+import { CoreOutput } from '../../common/dto/core-output.dto';
+import { Project } from '../entities/project.entity';
+
+export class GetAllProjectsOutput extends CoreOutput {
+  projects?: Project[];
+}
+
+export class GetProjectOutput extends CoreOutput {
+  project?: Project;
+}
