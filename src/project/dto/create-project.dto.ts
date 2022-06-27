fbdeec796e05326424593ba3810103e0daa4922e
wook <gus5427@naver.com>
2022-06-28 00:15:33 +0900

    Feat : add feature of updating a project

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { PickType } from '@nestjs/swagger';
-import { Project } from '../entities/project.entity';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { Project, ProjectStatus } from '../entities/project.entity';
 import { CoreOutput } from '../../common/dto/core-output.dto';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
 
 @Injectable()
 export class CreateProjectInput extends PickType(Project, [
@@ -14,6 +15,44 @@ export class CreateProjectInput extends PickType(Project, [
   'status',
   'total',
   'pricePerQuarter',
-]) {}
+]) {
+  @IsString()
+  @ApiProperty()
+  title: string;
+
+  @IsString()
+  @ApiProperty()
+  summary: string;
+
+  @IsString()
+  @ApiProperty()
+  content: string;
+
+  @IsString()
+  @ApiProperty()
+  address: string;
+
+  @IsString()
+  @ApiProperty()
+  thumbnailImage: string;
+
+  @IsString()
+  @ApiProperty()
+  deadline: Date;
+
+  @IsEnum(ProjectStatus, {
+    message: "status must be 'before' or 'progress' or 'end'",
+  })
+  @ApiProperty()
+  status: ProjectStatus;
+
+  @IsNumber()
+  @ApiProperty()
+  total: number;
+
+  @IsNumber()
+  @ApiProperty()
+  pricePerQuarter: number;
+}
 
 export class CreateProjectOutput extends CoreOutput {}
