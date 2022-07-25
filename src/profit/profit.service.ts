3486b2ac97accfd8b6bdc5321d0e6eeecd65124b
wook <gus5427@naver.com>
2022-07-25 21:19:08 +0900

    FEAT : add feature of serving point to user searched by phone number

--- src/profit/profit.service.ts
+++ src/profit/profit.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { Project } from '../project/entities/project.entity';
 
 @Injectable()
-export class ProfitService {}
+export class ProfitService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>
+  ) {}
+
+  async servePointsToUsers(pointsToBeAdded: number, phoneNumbers: string[]) {
+    let user: User;
+    let totalPoints: number;
+    console.log(phoneNumbers);
+    for (const phoneNumber of phoneNumbers) {
+      console.log(phoneNumber);
+      user = await this.userRepository.findOne({
+        where: {
+          phoneNumber,
+        },
+      });
+      if (user) {
+        totalPoints = user.points + pointsToBeAdded;
+        await this.userRepository.update(user.id, { points: totalPoints });
+        console.log('Find User');
+        console.log(`totalPoints : ${totalPoints}`);
+      }
+    }
+  }
+}
