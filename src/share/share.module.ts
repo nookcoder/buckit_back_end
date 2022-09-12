5270693f4d19b08289248affea9d894fb308fbc1
wook <gus5427@naver.com>
2022-09-12 17:41:10 +0900

    UPDATE update service.module

--- src/share/share.module.ts
+++ src/share/share.module.ts
@@ -3,10 +3,13 @@ import { ShareService } from './share.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Dividend } from './entities/dividend.entity';
 import { Share } from './entities/share.entity';
+import { ShareController } from './share.controller';
+import { ProjectModule } from '../project/project.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Share, Dividend])],
+  imports: [TypeOrmModule.forFeature([Share, Dividend]), ProjectModule],
   providers: [ShareService],
   exports: [ShareService],
+  controllers: [ShareController],
 })
 export class ShareModule {}
