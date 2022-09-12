4bf1a856acac25edb88fddfae54fbdd565b87eed
wook <gus5427@naver.com>
2022-09-12 17:37:31 +0900

    ADD : 블럭 포기 controller 추가

--- src/share/share.service.ts
+++ src/share/share.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { Dividend } from './entities/dividend.entity';
 import { User } from '../user/entities/user.entity';
 import { Project } from '../project/entities/project.entity';
+import { ProjectService } from '../project/project.service';
 
 @Injectable()
 export class ShareService {
@@ -12,7 +13,8 @@ export class ShareService {
     @InjectRepository(Share)
     private readonly shareRepository: Repository<Share>,
     @InjectRepository(Dividend)
-    private readonly dividendRepository: Repository<Dividend>
+    private readonly dividendRepository: Repository<Dividend>,
+    private readonly projectService: ProjectService
   ) {}
 
   private readonly logger = new Logger(ShareService.name);
@@ -27,6 +29,28 @@ export class ShareService {
       await this.shareRepository.save(share);
     } catch (error) {
       this.logger.error(error);
+    }
+  }
+
+  async giveUpShare(userId: number, shareUuid: string) {
+    try {
+      const share = await this.shareRepository.findOne({
+        where: {
+          share_uuid: shareUuid,
+        },
+        relations: ['project'],
+      });
+
+      await this.projectService.updateProjectWithPartialEntity(
+        share.project_id,
+        {
+          soldQuarter: share.project.soldQuarter - share.total_share_number,
+        }
+      );
+
+      await this.shareRepository.delete(share.id);
+    } catch (err) {
+      console.log(err);
     }
   }
 }
